10b021f810058677c9782655c2912f03e8bba9a8
Alijan <alijanrahimi24@yahoo.com>
2023-03-08 22:23:07 +0500

    Palindrome checking page completion

---
+++ Word Check/main.js
@@ -0,0 +1,19 @@
+
+const btn=document.querySelector('.btn');
+const result=document.querySelector('.result')
+
+
+btn.addEventListener('click', checkingText)
+function checkingText(){
+    let word=document.querySelector('.input-text').value;
+    let len=word.length;
+    let start=word.substring(0,Math.floor(len/2)).toLowerCase();
+    let end=word.substring(len - Math.floor(len/2)).toLowerCase();
+
+    let flip=end.split("").reverse().join("");
+    if(start==flip){
+        result.innerHTML=word.toUpperCase()+ " is a Palindrome";
+    }else{
+        result.innerHTML=word.toUpperCase()+ " is NOT a Palindrome";
+    }
+}
