2d10b71bdb381c22ccdf73a9baf1346de51c8c85
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-11-24 18:55:42 +0500

    add todo list

---
+++ javascript-practice/scriptes/todoList.js
@@ -0,0 +1,43 @@
+let todoArray = [
+];
+renderPage()
+
+function addTodo(event){
+    event.preventDefault()
+    let nameInput = document.querySelector('.name-input');
+    const dateInput = document.querySelector('.date-input');
+    name = nameInput.value;
+    todoDate = dateInput.value;
+
+    todoArray.push({
+        name,
+        todoDate
+    })
+
+    nameInput.value = "";
+
+    renderPage()
+}
+function renderPage (){
+    // console.log(todoArray)
+     let todoHTML = '';
+ 
+     for (let i=0; i<todoArray.length; i++){
+         const todoObj = todoArray[i];
+         const {name, todoDate} = todoObj;
+         console.log(todoObj)
+         const html = `<div><p>${name}  ${todoDate} <button onclick = "deleteTodo()">Delete</button></p>`;
+         todoHTML = todoHTML + html;
+     }
+ 
+     document.querySelector('.js-todo-list').innerHTML = todoHTML;
+ };
+
+
+ function deleteTodo(){
+    for (let i = 0; i<todoArray.length; i++){
+        todoArray.splice({i}, 1)
+    }
+
+    renderPage()
+ }
