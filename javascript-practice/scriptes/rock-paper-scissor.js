2820a55a00ab8f2fd7a88d91ce0443571add1dba
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-11-25 17:15:41 +0500

    add auto-play function for rock-paper-scissor game

--- javascript-practice/scriptes/rock-paper-scissor.js
+++ javascript-practice/scriptes/rock-paper-scissor.js
@@ -6,6 +6,22 @@ let score = JSON.parse(localStorage.getItem('score')) || {
 
   updateScoreElement();
 
+    let isAutoPlaying = false;
+    let intervalId;
+    function autoPlay(){
+      if (!isAutoPlaying){
+          intervalId = setInterval(function(){
+          const playerMove = pickComputerMove();
+          playGame(playerMove)
+          },1000)
+          isAutoPlaying = true;
+      }else{
+        clearInterval(intervalId)
+        isAutoPlaying = false;
+      }    
+    }
+
+
   function playGame(playerMove) {
     const computerMove = pickComputerMove();
 
